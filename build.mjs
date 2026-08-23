ad9e2864c88fa13233dc20f45a9f2cfd31033403
Zvonimir Šego <95874092+zvonimirsego@users.noreply.github.com>
2026-08-24 00:01:50 +0200

    making repo ready for deployment.

---
+++ build.mjs
@@ -0,0 +1,63 @@
+import {
+    rmSync,
+    mkdirSync,
+    cpSync,
+    copyFileSync
+} from "node:fs";
+
+// Delete old build
+rmSync("dist", {
+    recursive: true,
+    force: true
+});
+
+// Create directories
+mkdirSync("dist", {
+    recursive: true
+});
+
+mkdirSync("dist/vendor/bootstrap", {
+    recursive: true
+});
+
+mkdirSync("dist/vendor/bootstrap-icons/fonts", {
+    recursive: true
+});
+
+// Copy your website
+copyFileSync("index.html", "dist/index.html");
+
+cpSync("src", "dist/src", {
+    recursive: true
+});
+
+cpSync("img", "dist/img", {
+    recursive: true
+});
+
+// Copy Bootstrap from node_modules
+copyFileSync(
+    "node_modules/bootstrap/dist/css/bootstrap.min.css",
+    "dist/vendor/bootstrap/bootstrap.min.css"
+);
+
+copyFileSync(
+    "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
+    "dist/vendor/bootstrap/bootstrap.bundle.min.js"
+);
+
+// Copy Bootstrap Icons from node_modules
+copyFileSync(
+    "node_modules/bootstrap-icons/font/bootstrap-icons.min.css",
+    "dist/vendor/bootstrap-icons/bootstrap-icons.min.css"
+);
+
+cpSync(
+    "node_modules/bootstrap-icons/font/fonts",
+    "dist/vendor/bootstrap-icons/fonts",
+    {
+        recursive: true
+    }
+);
+
+console.log("Build completed.");
